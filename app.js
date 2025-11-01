54c17d74f31da79673dcadad9f4c5c3b822ebdfe
SaadDev <zwixozouine@gmail.com>
2025-11-01 16:11:02 +0100

    add necessary files and folders

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+//
+app.get("/", (req, res) => {
+    res.status(201).json({
+        message: "Testing step"
+    });
+});
+
+export default app;
---
+++ index.js
@@ -0,0 +1,8 @@
+// Running app from here
+import app from "./app.js";
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
